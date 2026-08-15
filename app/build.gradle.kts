2b78d46d8ab98c825aee16d3c77f2ab48fde426a
Ali <aliahadmd1@outlook.com>
2026-08-15 20:09:23 +0800

    Bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.aliahad.aichat"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
         buildConfigField(
             "String",
             "LLAMA_RUNTIME_REVISION",
