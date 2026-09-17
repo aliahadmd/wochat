614980834f6000db320021fdd767541b1a4ef7e5
Ali <aliahadmd1@outlook.com>
2026-09-18 04:30:30 +0800

    Release 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "com.aliahad.aichat"
         minSdk = 33
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.3.5"
+        versionCode = 15
+        versionName = "1.4.0"
         buildConfigField(
             "String",
             "LLAMA_RUNTIME_REVISION",
